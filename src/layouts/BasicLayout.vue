1f6250e48202397d40ff82e6cfba4c9d6fc1bd9b
kyle <903586678@qq.com>
2025-12-21 19:43:18 +0800

    1-commit

---
+++ src/layouts/BasicLayout.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import type { CSSProperties } from 'vue'
+</script>
+<template>
+  <div id="basicLayout">
+    <a-layout style="min-height: 100vh">
+      <a-layout-header>Header</a-layout-header>
+      <a-layout-content>Content</a-layout-content>
+      <a-layout-footer class="footer">
+        <a href="https://github.com/lynnkyle/picture-cloud-front"> github地址 by 林泽源 </a>
+      </a-layout-footer>
+    </a-layout>
+  </div>
+</template>
+<style scoped></style>
