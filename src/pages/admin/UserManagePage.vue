3118e9d0e44e82a612511f4b6ca902e0435ff2e6
LinZeyuan <903586678@qq.com>
2026-01-05 17:57:05 +0800

    4-commit

--- src/pages/admin/UserManagePage.vue
+++ src/pages/admin/UserManagePage.vue
@@ -1,59 +1,66 @@
 <script setup lang="ts">
-import { onMounted, reactive, ref } from 'vue'
+import { computed, onMounted, reactive, ref } from 'vue'
 import { listUserVoByPageUsingPost } from '@/api/userController.ts'
 import { message } from 'ant-design-vue'
-
+import dayjs from 'dayjs'
+// 表格属性
 const columns = [
   {
     title: 'id',
-    dataIndex: 'id',
+    dataIndex: 'id'
   },
   {
     title: '账号',
-    dataIndex: 'userAccount',
+    dataIndex: 'userAccount'
   },
   {
     title: '用户名',
-    dataIndex: 'userName',
+    dataIndex: 'userName'
   },
   {
     title: '头像',
-    dataIndex: 'userAvatar',
+    dataIndex: 'userAvatar'
   },
   {
     title: '简介',
-    dataIndex: 'userProfile',
+    dataIndex: 'userProfile'
   },
   {
     title: '用户角色',
-    dataIndex: 'userRole',
+    dataIndex: 'userRole'
   },
   {
     title: '创建时间',
-    dataIndex: 'createTime',
+    dataIndex: 'createTime'
   },
   {
     title: '更新时间',
-    dataIndex: 'updateTime',
+    dataIndex: 'updateTime'
   },
   {
     title: '操作',
-    key: 'action',
-  },
+    key: 'action'
+  }
 ]
-
 // 定义数据
 const total = ref(0)
 const dataList = ref<API.UserVO[]>([])
-// 搜索条件
 const searchParams = reactive<API.UserQueryRequest>({
   current: 1,
-  pageSize: 10,
+  pageSize: 10
+})
+const pagination = computed(() => {
+  return {
+    current: searchParams.current,
+    pageSize: searchParams.pageSize,
+    total: total.value,
+
+  }
 })
 // 获取数据
 const fetchData = async () => {
   const resp = await listUserVoByPageUsingPost({
-    ...searchParams,
+    ...searchParams
   })
   const res = resp.data
   if (res.code === 20000 && res.data) {
@@ -75,7 +82,7 @@ onMounted(() => {
     <a-table :columns="columns" :data-source="dataList">
       <template #bodyCell="{ column, record }">
         <template v-if="column.dataIndex === 'userAvatar'">
-          <a-image :src="record.userAvatar" :width="30" />
+          <a-image :src="record.userAvatar" :width="50" />
         </template>
         <template v-else-if="column.dataIndex === 'userRole'">
           <div v-if="record.userRole === 'admin'">
@@ -84,6 +91,19 @@ onMounted(() => {
           <div v-else>
             <a-tag color="blue">普通用户</a-tag>
           </div>
+        </template>
+        <template v-else-if="column.dataIndex === 'createTime'">
+          <div>
+            {{ dayjs(record.createTime).format('YYYY-MM-DD HH:mm:ss') }}
+          </div>
+        </template>
+        <template v-else-if="column.dataIndex === 'updateTime'">
+          <div>
+            {{ dayjs(record.updateTime).format('YYYY-MM-DD HH:mm:ss') }}
+          </div>
+        </template>
+        <template v-else-if="column.key === 'action'">
+          <a-button danger>删除</a-button>
         </template>
       </template>
     </a-table>
